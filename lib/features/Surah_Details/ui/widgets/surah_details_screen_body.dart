ca46883a6b2ade6a358079ed7c9aa38cf3eb2c11
OmarEidAwad <152607793+OmarEidAwad@users.noreply.github.com>
2026-03-12 04:14:14 +0300

    home feature

---
+++ lib/features/Surah_Details/ui/widgets/surah_details_screen_body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:quraanapp/core/theiming/colors.dart';
+import 'package:quraanapp/features/home/ui/widgets/custom_app_bar.dart';
+
+class SurahDetailsScreenBody extends StatelessWidget {
+  const SurahDetailsScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 22.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(
+            ontap: () {
+              Navigator.pop(context);
+            },
+            firstIcon: FontAwesomeIcons.arrowLeft,
+            title: '  Alfatiha',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/home/ui/widgets/custom_home_card.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:quraanapp/core/theiming/styles.dart';
+
+class CustomHomeCard extends StatelessWidget {
+  const CustomHomeCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 130.h,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.r),
+        image: DecorationImage(
+          image: AssetImage("assets/images/images_png/surah_background.png"),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 22.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                SvgPicture.asset(
+                  "assets/images/images_svg/small_mushaf_icon.svg",
+                ),
+                SizedBox(width: 8.w),
+                Text("Last Read", style: TextStyles.font14WhiteMedium),
+              ],
+            ),
+            Text(" Al-Fatiha", style: TextStyles.font22WhiteExtraBold),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Ayah No: 1", style: TextStyles.font14WhiteRegular),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
